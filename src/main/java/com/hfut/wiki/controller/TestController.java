bd0c2f305c74e02ce69ad87f65a371fcbca15e1b
zbc <3210191548@qq.com>
2024-01-01 21:49:06 +0800

    添加了all.sql

--- src/main/java/com/hfut/wiki/controller/TestController.java
+++ src/main/java/com/hfut/wiki/controller/TestController.java
@@ -1,13 +1,16 @@
 package com.hfut.wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+    @Value("${test.hello:TEST}")
+    private  String testHello;
 
     @RequestMapping("/hello")
     public String hello(){
-        return "Hello World!";
+        return testHello+ "Hello World!";
     }
 }
