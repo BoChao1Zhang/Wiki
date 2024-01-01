fb94fb9c08290171aaf27a8505b64fd68db5d681
zbc <3210191548@qq.com>
2024-01-01 20:46:11 +0800

    开发 Hello World 接口

--- src/main/java/com/hfut/wiki/WikiApplication.java
+++ src/main/java/com/hfut/wiki/WikiApplication.java
@@ -2,7 +2,9 @@ package com.hfut.wiki;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan("com.hfut")
 @SpringBootApplication
 public class WikiApplication {
 
